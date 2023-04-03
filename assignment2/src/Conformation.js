f344c3463421460bac4ef4a7f128cc514b037e97
Randy Nguyen <47833130+randyvan12@users.noreply.github.com>
2023-04-03 00:12:43 -0500

    added changing views using states.

---
+++ assignment2/src/Conformation.js
@@ -0,0 +1,9 @@
+export function Conformation(props) {
+    return (
+        <div>
+            <button onClick={props.changeViewToBrowse}>Temp button to Browse</button>
+            <h1>Hello World</h1>
+
+        </div>
+    )
+}
