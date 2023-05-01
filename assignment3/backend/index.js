ebb12f9e84d05262dcf6d55d0dbb7dbffdac80e5
Matthew Duncan <mlduncan@iastate.edu>
2023-04-30 19:25:02 -0500

    Change to new format, complete READ, UPDATE, and DELETE views

--- assignment3/backend/index.js
+++ assignment3/backend/index.js
@@ -3,7 +3,8 @@ var cors = require("cors");
 var app = express();
 var fs = require("fs");
 var bodyParser = require("body-parser");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
+
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -26,123 +27,180 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.get("/listProducts", async (req, res) => {
+// READ ALL
+app.get("/products/", async (req, res) => {
   await client.connect();
   console.log("Node connected successfully to GET MongoDB");
+
   const query = {};
   const results = await db
     .collection("fakestore_catalog")
     .find(query)
     .limit(100)
     .toArray();
+    
   console.log(results);
   res.status(200);
   res.send(results);
 });
 
-app.get("/:id", async (req, res) => {
-  const product = "product" + req.params.id + ".id";
-  const productId = Number(req.params.id);
-  console.log("Product to find :", product, " id :", productId);
+// READ
+app.get("/products/:id", async (req, res) => {
+  const _id = req.params.id;
+  console.log("Product id to find :", _id);
+
   await client.connect();
   console.log("Node connected successfully to GET-id MongoDB");
-  const query = { [product]: productId };
+  const query = { _id };
   const results = await db.collection("fakestore_catalog").findOne(query);
   console.log("Results :", results);
+
   if (!results) res.send("Not Found").status(404);
   else res.send(results).status(200);
 });
 
-app.post("/addProduct", async (req, res) => {
-  await client.connect();
-  const keys = Object.keys(req.body);
-  const values = Object.values(req.body);
-  const k = keys[0];
-  const v = values[0];
-  console.log("Keys :", k, " Values", v);
-  const newDocument = { _id: "4", [k]: [v] };
-  const results = await db.collection("fakestore_catalog").insertOne(newDocument);
-  res.status(200);
-  res.send(results);
-});
+// // CREATE
+// app.post("/products/:id", async (req, res) => {
+//   await client.connect();
+//   const keys = Object.keys(req.body);
+//   const values = Objects.values(req.body);
+//   const k = keys[0];
+//   const v = values[0];
+//   console.log("Keys :", k, " Values", v);
+//   const newDocument = { _id: "4", [k]: [v] };
+//   const results = await db.collection("fakestore_catalog").insertOne(newDocument);
+//   res.status(200);
+//   res.send(results);
+// });
 
-app.delete("/deleteProduct", async (req, res) => {
-  await client.connect();
-  const keys = Object.keys(req.body);
-  const k = keys[0];
-  const query = { [k]: { $exists: true } };
-  const results = await db.collection("fakestore_catalog").deleteOne(query);
-  res.status(200);
-  res.send(results);
-});
-
-app.post("/updatePrice", async (req, res) => {
-  const { id, price } = req.body;
-
-  if (!id || !price) {
-    res.status(400).send("Missing required fields: id and/or price");
+// DELETE
+app.delete("/products/:id", async (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.sendStatus(400);
     return;
   }
-
-  await client.connect();
-  console.log("Node connected successfully to updatePrice MongoDB");
-
-  const productKeyPattern = /^product\d+$/;
-
-  const docs = await db.collection("fakestore_catalog").find({}).toArray();
-  console.log('Docs:', docs); // Added log
-
-  let foundProductKey;
-  let foundDoc;
-  let foundProductIndex;
-
-  for (const doc of docs) {
-    const productKeys = Object.keys(doc).filter((key) => productKeyPattern.test(key));
-    console.log('ProductKeys:', productKeys); // Added log
-
-    for (const productKey of productKeys) {
-      const productIndex = doc[productKey].findIndex((product) => product.id === id);
-      
-      if (productIndex !== -1) {
-        foundProductKey = productKey;
-        foundDoc = doc;
-        foundProductIndex = productIndex;
-        break;
-      }
-    }
-
-    if (foundProductKey && foundDoc && foundProductIndex !== undefined) {
-      break;
-    }
-  }
-
-  console.log('FoundProductKey:', foundProductKey); // Added log
-  console.log('FoundDoc:', foundDoc); // Added log
-
-  if (!foundProductKey || !foundDoc) {
-    res.status(404).send("Item not found");
-    return;
-  }
-
-  const query = { _id: foundDoc._id };
-  const update = {
-    $set: {
-      [`${foundProductKey}.${foundProductIndex}.price`]: price,
-    },
-  };
-  const options = { returnOriginal: false };
 
   try {
-    const result = await db.collection("fakestore_catalog").findOneAndUpdate(query, update, options);
+    await client.connect();
 
-    if (!result.value) {
-      res.status(404).send("Item not found");
+    console.log(`Attempting to delete ${req.params.id}`);
+
+    const query = { _id: req.params.id };
+    const results = await db.collection("fakestore_catalog").deleteOne(query);
+    console.log(results)
+    if (results.deletedCount > 0) {
+      res.sendStatus(204);
+      return;
+    } else {
+      res.sendStatus(404);
+      return;
+    }
+  } catch (e) {
+    res.sendStatus(500);
+    return;
+  }
+});
+
+// UPDATE
+app.put("/products/:id", async (req, res) => {
+  const {price} = req.body;
+  try {
+    if (!price || isNaN(price) || price < 0) {
+      res.status(400).send("Invalid field: price");
       return;
     }
 
-    res.status(200).send(result.value);
-  } catch (error) {
-    console.error("Error updating price:", error);
-    res.status(500).send("Internal server error");
+    await client.connect();
+    console.log("Node connected successfully to updatePrice MongoDB");
+
+    const query = { _id: req.params.id };
+    const results = await db.collection("fakestore_catalog").findOneAndUpdate(query, {$set: { "price" : price}})
+    console.log("Updated product:", results);
+    res.status(200).send(results);
+  } catch (e) {
+    console.error(e)
+    res.sendStatus(500)
   }
 });
+
+// // DELETE
+// app.delete("/products/:id", async (req, res) => {
+//   await client.connect();
+//   const keys = Object.keys(req.body);
+//   const k = keys[0];
+//   const query = { [k]: { $exists: true } };
+//   const results = await db.collection("fakestore_catalog").deleteOne(query);
+//   res.status(200);
+//   res.send(results);
+// });
+
+// app.post("/products/:_id", async (req, res) => {
+//   const { _id: id, price } = req.body;
+
+//   if (!price) {
+//     res.status(400).send("Missing required fields: price");
+//     return;
+//   }
+
+//   await client.connect();
+//   console.log("Node connected successfully to updatePrice MongoDB");
+
+//   const productKeyPattern = /^product\d+$/;
+
+//   const docs = await db.collection("fakestore_catalog").find({}).toArray();
+//   console.log('Docs:', docs); // Added log
+
+//   let foundProductKey;
+//   let foundDoc;
+//   let foundProductIndex;
+
+//   for (const doc of docs) {
+//     const productKeys = Object.keys(doc).filter((key) => productKeyPattern.test(key));
+//     console.log('ProductKeys:', productKeys); // Added log
+
+//     for (const productKey of productKeys) {
+//       const productIndex = doc[productKey].findIndex((product) => product.id === id);
+      
+//       if (productIndex !== -1) {
+//         foundProductKey = productKey;
+//         foundDoc = doc;
+//         foundProductIndex = productIndex;
+//         break;
+//       }
+//     }
+
+//     if (foundProductKey && foundDoc && foundProductIndex !== undefined) {
+//       break;
+//     }
+//   }
+
+//   console.log('FoundProductKey:', foundProductKey); // Added log
+//   console.log('FoundDoc:', foundDoc); // Added log
+
+//   if (!foundProductKey || !foundDoc) {
+//     res.status(404).send("Item not found");
+//     return;
+//   }
+
+//   const query = { _id: foundDoc._id };
+//   const update = {
+//     $set: {
+//       [`${foundProductKey}.${foundProductIndex}.price`]: price,
+//     },
+//   };
+//   const options = { returnOriginal: false };
+
+//   try {
+//     const result = await db.collection("fakestore_catalog").findOneAndUpdate(query, update, options);
+
+//     if (!result.value) {
+//       res.status(404).send("Item not found");
+//       return;
+//     }
+
+//     res.status(200).send(result.value);
+//   } catch (error) {
+//     console.error("Error updating price:", error);
+//     res.status(500).send("Internal server error");
+//   }
+// });
--- assignment3/frontend/src/App.js
+++ assignment3/frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, Children } from 'react';
 
+const SERVER_ADDR = "localhost:8081"
 
 
 function App() {
@@ -12,13 +13,9 @@ function App() {
   //for the search bar
   const [input, setInput] = useState("");
   const [updatedPrices, setUpdatedPrices] = useState({});
-
-  // function Change(event) {
-  //   setInput(event.target.value)
-  // }
   
   const fetchData = async () => {
-    fetch('http://localhost:8081/listProducts')
+    fetch(`http://${SERVER_ADDR}/products`)
       .then(response => response.json())
       .then(json => setData(json))
       .catch(error => console.error(error));
@@ -29,80 +26,11 @@ function App() {
     fetchData();
   }, []);
 
-  const handleSearch = (event) => {
-    event.preventDefault();
-    fetch(`http://localhost:8081/${input}`)
-      .then((response) => response.json())
-      .then((json) => setSearchResults(json))
-      .catch((error) => console.error(error));
-  };
-
   const handleLogoClick = (event) => {
     event.preventDefault();
     fetchData();
     window.location.reload();
   };
-
-  // const handlePriceUpdate = async (itemId, updatedPrice) => {
-  //   console.log(`Updated price for item ${itemId}: ${updatedPrice}`);
-
-  //   try {
-  //     const response = await fetch("http://localhost:8081/updatePrice", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         id: itemId,
-  //         price: parseFloat(updatedPrice),
-  //       }),
-  //     });
-
-  //     if (!response.ok) {
-  //       throw new Error(`Failed to update price: ${response.statusText}`);
-  //     }
-
-  //     const result = await response.json();
-  //     console.log("Price updated successfully:", result);
-
-  //     // Refetch the data and update the state
-  //     fetchData();
-  //   } catch (error) {
-  //     console.error("Error updating price:", error);
-  //   }
-  // };
-
-  // const handleDeleteProduct = async (itemId) => {
-  //   console.log(`Deleting item ${itemId}`);
-
-  //   try {
-  //     const response = await fetch("http://localhost:8081/deleteProduct", {
-  //       method: "DELETE",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         product1: {
-  //           id: itemId,
-  //         },
-  //       }),
-  //     });
-
-  //     if (!response.ok) {
-  //       throw new Error(`Failed to delete product: ${response.statusText}`);
-  //     }
-
-  //     const result = await response.json();
-  //     console.log("Product deleted successfully:", result);
-
-  //     // Refetch the data and update the state
-  //     fetchData();
-  //   } catch (error) {
-  //     console.error("Error deleting product:", error);
-  //   }
-  // };
-
-  
 
   return (
     <div className="App">
@@ -147,35 +75,19 @@ function App() {
                   <button class={(view==="credits" ? "active" : "") + " nav-link"} onClick={() => {setView("credits")}}>Credits</button>
                 </li>
               </ul>
-            
-            <form class="navbar-brand d-flex align-items-center" onSubmit={handleSearch}>
-              <input
-                type="search"
-                class="form-control"
-                placeholder="Search..."
-                aria-label="Search"
-                value={input}
-                onChange={(event) => setInput(event.target.value)}
-              />
-              <button type="submit" class="btn btn-primary">
-                Search
-              </button>
-            </form>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader"
-              aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
           </div>
         </div>
       </header>
-        {view ==="create" && <CreateView />}
-        {view ==="read" && <ReadView data={data} />}
-        {view ==="update" && <UpdateView />}
-        {view ==="delete" && <DeleteView />}
+        {view ==="create"  && <CreateView />}
+        {view ==="read"    && <ReadView data={data} />}
+        {view ==="update"  && <UpdateView />}
+        {view ==="delete"  && <DeleteView />}
         {view ==="credits" && <CreditsView />}
     </div>
   );
 }
+
+/* Views made from functional components */
 
 function CreateView() {
   return (
@@ -195,16 +107,9 @@ function ReadView(params) {
           <div class="container">
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
               {
-                 data.map((productGroup) => {
-                  // Find the key in the product group that starts with "product"
-                  const productKey = Object.keys(productGroup).find((key) =>
-                    key.startsWith("product")
-                  );
-                  // Extract the products from the product group using the product key
-                  const products = productGroup[productKey];
-
-                  // Render each product in the products array
-                  return products.map(item => <ItemCard item={item} /> );
+                 data.map((product) => {
+                  // console.log("product:", product)
+                  return <ProductCard item={product} />
                 })
                   }
             </div>
@@ -218,19 +123,131 @@ function ReadView(params) {
 }
 
 function UpdateView() {
-  return (
-    <main>
-      update view
+   const handlePriceUpdate = async (e, item, updatedPrice) => {
+    e.preventDefault()
+    e.stopPropagation()
+    console.log(`Updating price for item ${item._id}: ${updatedPrice}`);
+
+      try {
+        const response = await fetch(`http://${SERVER_ADDR}/products/${item._id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id: item._id,
+            price: parseFloat(updatedPrice),
+          }),
+        });
+
+        if (!response.ok) {
+          throw new Error(`Failed to update price: ${response.statusText}`);
+        }
+
+        const result = await response.json();
+        console.log("Price updated successfully:", result);
+
+        // Refetch the data and update the state
+        // fetchData();
+      } catch (error) {
+        console.error("Error updating price:", error);
+      }
+    };
+
+  const [item, setItem] = useState(null);
+  const priceInput = useRef(null);
+
+  console.log("item:", item)
+  
+  if (item) {
+    return (
+      <main>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <ProductCard item={item}>
+        <form class="d-flex justify-content-between align-items-center" onSubmit={
+          (e) => {
+            console.log(e);
+            handlePriceUpdate(e, item, priceInput.current.value);}
+          }>
+          <input type="number"
+            placeholder={item.price}
+            id="new-price"
+            ref={priceInput}
+            min="0"
+            step="0.01"
+            className="form-control"
+          />
+          <button type="submit" className="btn btn-primary" > Update </button>
+          </form>
+        </ProductCard>
+        </div>
+      </main>
+    )
+  }
+  else return (
+    <main className="centered-cont">
+        <ItemSearch callback={setItem} />
     </main>
   )
 }
 
 function DeleteView() {
-  return (
-    <main>
-      delete view
-    </main>
-  )
+  const handleDeleteProduct = async (e, itemId) => {
+    e.preventDefault();
+    console.log("Attempting to delete: ", itemId);
+    try {
+        const response = await fetch(`http://${SERVER_ADDR}/products/${itemId}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            product1: {
+              id: itemId,
+            },
+          }),
+        });
+
+        if (!response.ok) {
+          throw new Error(`Failed to delete product: ${response.statusText}`);
+        }
+
+        const result = await response.json();
+        console.log("Product deleted successfully:", result);
+
+      } catch (error) {
+        console.error("Error deleting product:", error);
+      }
+  };
+
+
+ const [item, setItem] = useState(null);
+ const priceInput = useRef(null);
+
+ console.log("item:", item)
+ 
+ if (item) {
+   return (
+     <main>
+       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+       <ProductCard item={item}>
+       <form class="d-flex justify-content-between align-items-center" onSubmit={
+         (e) => {
+           console.log(e);
+           handleDeleteProduct(e, item._id);
+          }}>
+         <button type="submit" className="btn btn-danger" >Confirm Delete</button>
+         </form>
+       </ProductCard>
+       </div>
+     </main>
+   )
+ }
+ else return (
+   <main className="centered-cont">
+       <ItemSearch callback={setItem} />
+   </main>
+ )
 }
 
 function CreditsView() {
@@ -243,21 +260,59 @@ function CreditsView() {
   );
 }
 
-function ItemCard({item}) {
-  return (
-  <div key={item.id} class="col">
-    <div class="card shadow-sm">
-      <img src={item.image} className="bd-placeholder-img card-img-top" alt={item.title} />
-      <div class="card-body">
-        <h5 class="card-title">{item.title}</h5>
-        <p class="card-text">{item.description}</p>
-        <p class="card-text">${item.price}</p>
-        <p class="card-text">Category: {item.category}</p>
-        <p class="card-text">{item.rating.rate} out of {item.rating.count} reviews</p>
+/* Other components for modularization */
 
+function ProductCard({item, children}) {
+  const f = children
+  // console.log("rendering product:", item.title)
+  // console.log("children:", f)
+  return (
+  <div key={item.id} className="col">
+    <div className="card shadow-sm">
+      <img src={item.image} className="bd-placeholder-img card-img-top" alt={item.title} />
+      <div className="card-body">
+        <h5 className="card-title">{item.title}</h5>
+        <p className="card-text">{item.description}</p>
+        <p className="card-text">${item.price}</p>
+        <p className="card-text">Category: {item.category}</p>
+        <p className="card-text">{item.rating.rate} out of {item.rating.count} reviews</p>
+        {f || ""}
       </div>
     </div>
   </div> )
 }
 
+function ItemSearch({callback}) {
+  const field = useRef(null);
+
+  return (
+    <form className="navbar-brand d-flex align-items-center" style={{width: "220px", }} onSubmit={(e) => handleSubmit(e, field.current, callback)}>
+    <input
+      type="search"
+      className="form-control"
+      id="id-input"
+      ref={field}
+      placeholder="Id"
+      aria-label="Search"
+    />
+    <button type="submit" class="btn btn-primary">
+      Search by ID
+    </button>
+  </form>
+  )
+}
+
+const handleSubmit = (event, field, callback) => {
+  event.preventDefault();
+  const id = field.value;
+
+  console.log(field)
+
+  console.log("Selected id: ", id)
+
+  fetch(`http://${SERVER_ADDR}/products/${id}`)
+  .then(response => response.json())
+  .then(json => callback(json))
+  .catch(error => console.error(error));
+}
 export default App;
