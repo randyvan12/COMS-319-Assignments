39c67b018ae42e2c92d71c35edeceea2edfa0ec6
Matthew Duncan <mlduncan@iastate.edu>
2023-05-05 15:11:11 -0500

    Add CRUD methods for records

--- Final/Backend/index.js
+++ Final/Backend/index.js
@@ -39,6 +39,100 @@ app.get("/getData", async (req, res) => {
   res.send(result);
 });
 
+// get all records
+app.get("/records", async (req, res) => {
+  await client.connect();
+
+  console.log("Node connected successfully to GET MongoDB");
+
+  const result = await db.collection("records").find().toArray();
+  
+  if (!result) {
+    res.status(404).send("No records found");
+    return;
+  }
+  console.log("Found record: ", result);
+  res.status(200);
+  res.send(result);
+});
+
+// add a record
+app.post("/records/", async (req, res) => {
+  const { date, temperature_f, temperature_c, humidity } = req.body;
+
+  if (!date || !temperature_f || !temperature_c || !humidity) {
+    res.status(400).send("Missing required fields: date, temperature_f, temperature_c, and/or humidity");
+    return;
+  }
+
+  await client.connect();
+
+  const newDocument = {
+    date: date,
+    temperature_f: temperature_f,
+    temperature_c: temperature_c,
+    humidity: humidity,
+  };
+
+  const result = await db.collection("records").insertOne(newDocument);
+  res.status(200);
+  res.send(result);
+});
+
+
+// update an existing record
+app.put("/records/:rid", async (req, res) => {
+  const _id = req.params.rid;
+  const { date, temperature_f, temperature_c, humidity } = req.body;
+
+  if (!date || !temperature_f || !temperature_c || !humidity) {
+    res.status(400).send("Missing required fields: date, temperature_f, temperature_c, and/or humidity");
+    return;
+  }
+
+  await client.connect();
+  console.log("Node connected successfully to updateData MongoDB");
+
+  const query = { _id };
+  const update = {
+    $set: {
+      date: date,
+      temperature_f: temperature_f,
+      temperature_c: temperature_c,
+      humidity: humidity,
+    },
+  };
+  const options = { returnOriginal: false };
+
+  try {
+    const result = await db.collection("records").findOneAndUpdate(query, update, options);
+
+    if (!result.value) {
+      res.status(404).send("Item not found");
+      return;
+    }
+
+    res.status(200).send(result.value);
+  } catch (error) {
+    console.error("Error updating data:", error);
+    res.status(500).send("Internal server error");
+  }
+});
+
+// remove a record
+app.delete("/records/:rid", async (req, res) => {
+    const _id = req.params.rid;
+
+    await client.connect();
+  
+    const query = { _id: _id };
+    const result = await db.collection("records").deleteOne(query);
+
+    res.status(200);
+    res.send(result);
+});
+  
+
 app.post("/addData", async (req, res) => {
   const { date, temperature_f, temperature_c, humidity } = req.body;
 
@@ -63,56 +157,56 @@ app.post("/addData", async (req, res) => {
   res.send(result);
 });
 
-app.delete("/deleteData", async (req, res) => {
-  const { _id } = req.body;
+// app.delete("/deleteData", async (req, res) => {
+//   const { _id } = req.body;
 
-  if (!_id) {
-    res.status(400).send("Missing required field: _id");
-    return;
-  }
+//   if (!_id) {
+//     res.status(400).send("Missing required field: _id");
+//     return;
+//   }
 
-  await client.connect();
+//   await client.connect();
 
-  const query = { _id: _id };
-  const result = await db.collection("data").deleteOne(query);
-  res.status(200);
-  res.send(result);
-});
+//   const query = { _id: _id };
+//   const result = await db.collection("data").deleteOne(query);
+//   res.status(200);
+//   res.send(result);
+// });
 
 
-app.post("/updateData", async (req, res) => {
-  const { date, temperature_f, temperature_c, humidity } = req.body;
+// app.post("/updateData", async (req, res) => {
+//   const { date, temperature_f, temperature_c, humidity } = req.body;
 
-  if (!date || !temperature_f || !temperature_c || !humidity) {
-    res.status(400).send("Missing required fields: date, temperature_f, temperature_c, and/or humidity");
-    return;
-  }
+//   if (!date || !temperature_f || !temperature_c || !humidity) {
+//     res.status(400).send("Missing required fields: date, temperature_f, temperature_c, and/or humidity");
+//     return;
+//   }
 
-  await client.connect();
-  console.log("Node connected successfully to updateData MongoDB");
+//   await client.connect();
+//   console.log("Node connected successfully to updateData MongoDB");
 
-  const query = { _id: "1" };
-  const update = {
-    $set: {
-      date: date,
-      temperature_f: temperature_f,
-      temperature_c: temperature_c,
-      humidity: humidity,
-    },
-  };
-  const options = { returnOriginal: false };
+//   const query = { _id: "1" };
+//   const update = {
+//     $set: {
+//       date: date,
+//       temperature_f: temperature_f,
+//       temperature_c: temperature_c,
+//       humidity: humidity,
+//     },
+//   };
+//   const options = { returnOriginal: false };
 
-  try {
-    const result = await db.collection("data").findOneAndUpdate(query, update, options);
+//   try {
+//     const result = await db.collection("data").findOneAndUpdate(query, update, options);
 
-    if (!result.value) {
-      res.status(404).send("Item not found");
-      return;
-    }
+//     if (!result.value) {
+//       res.status(404).send("Item not found");
+//       return;
+//     }
 
-    res.status(200).send(result.value);
-  } catch (error) {
-    console.error("Error updating data:", error);
-    res.status(500).send("Internal server error");
-  }
-});
+//     res.status(200).send(result.value);
+//   } catch (error) {
+//     console.error("Error updating data:", error);
+//     res.status(500).send("Internal server error");
+//   }
+// });
