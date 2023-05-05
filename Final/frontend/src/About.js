9a9ca4f4ae68bd0380c2762e048a3bb54e707ee5
Matthew Duncan <mlduncan@iastate.edu>
2023-05-05 12:57:26 -0500

    Add professor to about

--- Final/frontend/src/About.js
+++ Final/frontend/src/About.js
@@ -9,10 +9,11 @@ export function About() {
                 </header>
                 <div class="row align-items-md-stretch">
                     <div class="col-md-6">
-                        <div class="h-100 p-5 rounded-3" style={{ backgroundColor: '#3A98B9', }}>
+                        <div class="h-100 p-5 border rounded-3" style={{ backgroundColor: '#3A98B9', }}>
                             <h2>Randy Nguyen</h2>
                             <p>Currently a Junior majoring in Computer Science. I enjoy working with computers and learning
                                 new techonology!</p>
+                            <strong>contact: </strong><a href="mailto: rvnguyen@iastate.edu">rvnguyen@iastate.edu</a>
 
                         </div>
                     </div>
@@ -21,6 +22,15 @@ export function About() {
                             <h2>Matthew Duncan</h2>
                             <p>I am a Senior at majoring in Computer Science. Working on the backend server for my senior
                                 project has kept me very busy this semester!</p>
+                            <strong>contact: </strong><a href="mailto: mlduncan@iastate.edu">mlduncan@iastate.edu</a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="h-100 p-5 border rounded-3" style={{ backgroundColor: '#FFF1DC', }}>
+                            <h2> Dr. Abraham N. Aldaco Gastelum</h2>
+                            <p>Our professor for this course.</p>
+                            <strong>contact: </strong><a href="mailto: aaldaco@iastate.edu">aaldaco@iastate.edu</a>
                         </div>
                     </div>
                 </div>
