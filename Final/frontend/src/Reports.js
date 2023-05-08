489e4baae1da91f9aafeebab0c815e5b51cd3172
Randy Nguyen <47833130+randyvan12@users.noreply.github.com>
2023-05-08 15:58:27 -0500

    Almost finished doc and update records page

--- Final/frontend/src/Reports.js
+++ Final/frontend/src/Reports.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import './Reports.css';
 
 export function Reports() {
     let [items, setItems] = useState([]);
@@ -8,30 +9,30 @@ export function Reports() {
         const id = items[index]._id;
 
         fetch(`http://${process.env.REACT_APP_URL}/records/${id}`, { method: 'DELETE' })
-        .then((res) => {
-            console.log("delete res:", res.body);
-        })
-        .then(res => res.json())
-        .catch((err) => {
-            console.log('error:' + err);
-        });
-        
+            .then((res) => {
+                console.log("delete res:", res.body);
+            })
+            .then(res => res.json())
+            .catch((err) => {
+                console.log('error:' + err);
+            });
+
         items.splice(index, 1);
         setItems([...items]);
     };
 
     useEffect(() => {
-    fetch(`http://${process.env.REACT_APP_URL}/records`)
-        .then((response) => response.json())
-        .then((data) => {
-            setItems([...data]);
-            console.log("items:", data);
-        })
-        .catch((err) => {
-            console.log('error:' + err);
-        });
+        fetch(`http://${process.env.REACT_APP_URL}/records`)
+            .then((response) => response.json())
+            .then((data) => {
+                setItems([...data]);
+                console.log("items:", data);
+            })
+            .catch((err) => {
+                console.log('error:' + err);
+            });
     }, []);
- 
+
     const update = (index, note) => {
         console.log(`${index}: `, note);
         let item = items[index];
@@ -41,81 +42,93 @@ export function Reports() {
 
         fetch(`http://${process.env.REACT_APP_URL}/records/${item._id}`, {
             method: 'PUT',
-            headers: {"Content-Type": "application/json"},
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify(item)
         })
-        .then((res) => {
-            console.log("delete res:", res.body);
-        })
-        .then(res => res.json())
-        .catch((err) => {
-            console.log('error:' + err);
-        });
+            .then((res) => {
+                console.log("delete res:", res.body);
+            })
+            .then(res => res.json())
+            .catch((err) => {
+                console.log('error:' + err);
+            });
 
         setItems([...items]);
     };
 
     return (
         <main>
-            <div class="container">
-              <ReportHeader />
-                {
-                    items.map((i, index) => <ReportRow item={i} initNote={i.note} id={i._id} date={i.date} tempf={i.temperature_f} tempc={i.temperature_c} humidity={i.humidity} remove={() => remove(index)} handleUpdate={(note) => update(index, note)} />)
-                }
+            <div className="container mt-5">
+                <h2>Saved Records</h2>
+                <table className="table table-bordered table-hover">
+                    <ReportHeader />
+                    <tbody>
+                        {items.map((i, index) => (
+                            <ReportRow
+                                item={i}
+                                initNote={i.note}
+                                id={i._id}
+                                date={i.date}
+                                tempf={i.temperature_f}
+                                tempc={i.temperature_c}
+                                humidity={i.humidity}
+                                remove={() => remove(index)}
+                                handleUpdate={(note) => update(index, note)}
+                            />
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </main>
-    )
+    );
 }
 
 function ReportHeader() {
-    return (        
-    <div class="row">
-        <div class="col-3">
-            Timestamp
-        </div>
-        <div class="col">
-            Fahrenheit
-        </div>
-        <div class="col">
-            Celcius
-        </div>
-        <div class="col">
-            Humidity
-        </div>
-        <div class="col-3">
-            Note
-        </div>
-        <div class="col-2 ml-auto">
-            Controls
-        </div>
-    </div>
-);
+    return (
+        <thead>
+            <tr>
+                <th>Timestamp</th>
+                <th>Fahrenheit</th>
+                <th>Celcius</th>
+                <th>Humidity</th>
+                <th>Note</th>
+                <th>Controls</th>
+            </tr>
+        </thead>
+    );
 }
 
-function ReportRow({ item , remove, handleUpdate}) {
+function ReportRow({ item, remove, handleUpdate }) {
     let noteRef = useRef(null);
 
     return (
-        <div class="row" key={item._id}>
-            <div class="col-3">
-                {item.date}
-            </div>
-            <div class="col">
-                {item.temperature_f}
-            </div>
-            <div class="col">
-                {item.temperature_c}
-            </div>
-            <div class="col">
-                {item.humidity}
-            </div>
-            <div class="col-3">
-                <input type="text" maxLength={32} size={22} ref={noteRef} defaultValue={item.note}></input>
-            </div>
-            <div class="col-2 ml-auto">
-                <button type="button" class="btn btn-primary" onClick={e => handleUpdate(noteRef.current.value)}>Update</button>
-                <button type="button" class="btn btn-danger" onClick={remove}>Remove</button>
-            </div>
-        </div>
-    )
+        <tr key={item._id}>
+            <td>{item.date}</td>
+            <td>{item.temperature_f}</td>
+            <td>{item.temperature_c}</td>
+            <td>{item.humidity}</td>
+            <td>
+                <input
+                    type="text"
+                    maxLength={32}
+                    size={22}
+                    ref={noteRef}
+                    defaultValue={item.note}
+                    className="form-control"
+                ></input>
+            </td>
+            <td>
+                <button
+                    type="button"
+                    className="btn btn-primary mr-2"
+                    onClick={(e) => handleUpdate(noteRef.current.value)}
+                >
+                    Update
+                </button>
+                <button type="button" className="btn btn-danger" onClick={remove}>
+                    Remove
+                </button>
+            </td>
+        </tr>
+    );
 }
